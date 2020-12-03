9277bb01594a5a1abc48b953a686c53307d366c8
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2020-12-03 09:28:22 +0530

    Day 1 Puzzle 1

---
+++ src/main/scala/day01/FindTwoNumbersWhoseSumIsX.scala
@@ -0,0 +1,55 @@
+package day01
+
+import scala.io.Source
+
+object FindTwoNumbersWhoseSumIsX {
+
+  case class Solution(num1:Int, num2:Int) {
+    def result():Int = num1 * num2
+
+    override def toString: String = s"Num1 = $num1, Num2 = $num2, Result = $result"
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val desiredSum = 2020
+    val solution1 = solveUsingHashSet(desiredSum, "day01-input.txt").map(_.toString).getOrElse("No Result")
+    println(solution1)
+    val solution2 = solveUsingSortAndLoop(desiredSum, "day01-input.txt").map(_.toString).getOrElse("No Result")
+    println(solution2)
+  }
+
+  private def solveUsingHashSet(desiredSum: Int, fileName: String): Option[Solution] = {
+    val lines: Set[Int] = Source.fromResource(fileName).getLines().map(_.toInt).toSet
+
+    val matchingResults = for {
+      input <- Source.fromResource(fileName).getLines().map(_.toInt)
+      num = input.toInt
+      if (lines.contains(desiredSum - num))
+    } yield Solution(num, desiredSum-num)
+
+    matchingResults.toList.headOption
+  }
+
+  private def solveUsingSortAndLoop(desiredSum: Int, fileName: String): Option[Solution] = {
+    val lines: List[Int] = Source.fromResource(fileName).getLines().map(_.toInt).toList.sorted
+
+    var i = 0
+    var j = lines.size-1
+
+    while(i <= lines.size-1 && j >= 0) {
+      val currentSum = lines(i) + lines(j)
+      if(currentSum == desiredSum)
+        return Option(Solution(lines(i), lines(j)))
+      else if (currentSum < desiredSum)
+        i = i + 1
+      else
+        j = j - 1
+    }
+    Option.empty
+  }
+
+
+}
+
+
